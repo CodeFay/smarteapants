ee083a49895dbfbe15392b76e75266da022db411
ckb <ckingbailey@gmail.com>
2018-12-15 17:16:19 -0800

    PascalCase GamePicker.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Axios from 'axios';
 
-import GamePicker from '../components/gamePicker'
+import GamePicker from '../components/GamePicker'
 import Layout from '../hoc/Layout'
 
 class Index extends React.Component {
