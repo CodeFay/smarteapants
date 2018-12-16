03cb1c9234f72764927f0b9e0b5233da57df6fab
ckb <ckingbailey@gmail.com>
2018-12-16 13:32:34 -0800

    in development, grab env json in webpack to get fauna key

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 var webpack = require("webpack");
 
+const define = {
+  "global.GENTLY": false
+}
+
+if (process.env.NODE_ENV === 'development') {
+  const env = require("./.env.development.json")
+  define['process.env.FAUNA_KEY'] = JSON.stringify(env.FAUNA_KEY)
+}
 /* fix for https://medium.com/@danbruder/typeerror-require-is-not-a-function-webpack-faunadb-6e785858d23b */
 module.exports = {
   plugins: [
-    new webpack.DefinePlugin({ "global.GENTLY": false })
+    new webpack.DefinePlugin(define)
   ],
   node: {
     __dirname: true,
